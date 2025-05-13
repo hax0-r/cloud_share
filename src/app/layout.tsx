438bafa2fbdad66bc38920923f798e9867c71ad8
Muhammad Talha <talha18513@gmail.com>
2025-05-13 15:02:35 +0500

    some text and style issue resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins, Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 
